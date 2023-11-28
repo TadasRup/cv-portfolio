a9f90521068e776e827a1b5c59f5b847fe93c98c
Kevinas <129725556+TadasRup@users.noreply.github.com>
2023-11-28 18:29:02 +0000

    Working DNS email with form with domain email

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,10 +1,28 @@
-import { Resend } from 'resend';
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
 
-const resend = new Resend('re_PAdnx2zT_2ULSssfGbj9kabkH1KejNwb5');
+const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
 
-resend.emails.send({
-  from: 'onboarding@resend.dev',
-  to: 'tadas.msp@gmail.com',
-  subject: 'Hello World',
-  html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
-});
+export async function POST(req, res) {
+  const { email, subject, message } = await req.json();
+  console.log(email, subject, message);
+  try {
+    const data = await resend.emails.send({
+      from: fromEmail,
+      to: [fromEmail, email],
+      subject: subject,
+      react: (
+        <>
+          <h1>{subject}</h1>
+          <p>Thank you for contacting us!</p>
+          <p>New message submitted:</p>
+          <p>{message}</p>
+        </>
+      ),
+    });
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 //import GithubIcon from "../../../public/github-icon.svg";
 //import LinkedinIcon from "../../../public/linkedin-icon.svg";
@@ -5,6 +6,32 @@ import Link from "next/link";
 import Image from "next/image";
 
 const EmailSection = () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            email: e.target.email.value,
+            subject: e.target.subject.value,
+            message: e.target.message.value,
+        }
+    const JSONdata = JSON.stringify(data);
+    const endpoint = "/api/send";
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSONdata,
+    }
+
+    const response = await fetch(endpoint, options);
+    const resData = await response.json();
+    console.log(resData);
+
+    if (response.status === 200) {
+        console.log('Message sent.')
+    }
+    }
   return (
     <section
     id="contact"
@@ -25,14 +52,16 @@ const EmailSection = () => {
             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" style={{ fill: 'white' }}>
             <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
         </svg>
-        </Link>   
+        </Link>
+        <Link href="https://linkedin.com"k>
             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" style={{ fill: 'white' }}>
             <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z" />
         </svg>
+        </Link>   
         </div>
       </div>
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>    
           <div class="mb-6">
             <label
               for="email"
@@ -41,6 +70,7 @@ const EmailSection = () => {
               Your email
             </label>
             <input
+              name="email"
               type="email"
               id="email"
               class="bg-gray-[#18191E] border border-[#33353F] bg-[#18191E] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg  block w-full p-2.5"
@@ -56,6 +86,7 @@ const EmailSection = () => {
               Subject
             </label>
             <input
+              name="subject"
               type="text"
               id="subject"
               class="bg-gray-[#18191E] border border-[#33353F] bg-[#18191E] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg  block w-full p-2.5"
@@ -75,15 +106,14 @@ const EmailSection = () => {
               placeholder="Let's talk about..."
             />
           </div>
-          <div class="mb-6">
-            <button
-              type="submit"
-              class="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
-            >
-              {" "}
-              Send message{" "}
+          <div className="mb-6">
+             <button
+            type="submit"
+            className="bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+                >
+                Send me a message
             </button>
-          </div>
+        </div>
         </form>
       </div>
     </section>
